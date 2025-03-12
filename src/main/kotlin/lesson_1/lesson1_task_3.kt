c1e182f72bab67eb61619f038a91127de374bf69
Nordone <kaldstring@gmail.com>
2025-03-12 17:52:45 +0300

    KS-1-4-variables

--- src/main/kotlin/lesson_1/lesson1_task_3.kt
+++ src/main/kotlin/lesson_1/lesson1_task_3.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_1
 
-fun main(){
-    val year = 1961
-    var hour = 9
-    var minute = 7
+fun main() {
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
 
     println("$year\n$hour\n$minute")
-    hour=10
-    minute=55
+    hour = "10"
+    minute = "55"
     print("$hour:$minute")
 
 }
