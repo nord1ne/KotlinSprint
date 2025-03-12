c4b87b3ff32487c674c37a5781467e9b15681783
Nordone <kaldstring@gmail.com>
2025-03-12 17:10:32 +0300

    KS-1-3 Gagarin task

---
+++ src/main/kotlin/lesson_1/lesson1_task_3.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_1
+
+fun main(){
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println("$year\n$hour\n$minute")
+    hour=10
+    minute=55
+    print("$hour:$minute")
+
+}
