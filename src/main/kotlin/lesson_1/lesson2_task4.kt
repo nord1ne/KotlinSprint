6003c6deb91dec181450635fc9aa1f1f868fa2dd
Nordone <kaldstring@gmail.com>
2025-03-13 13:43:28 +0300

    KS-2-4-ore-buff

---
+++ src/main/kotlin/lesson_1/lesson2_task4.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main() {
+    var crystalOre = 7
+    var metalOre = 11
+    val buff = 0.2
+
+    println(
+        """
+        Количество бонусной кристаллической руды: ${(crystalOre * buff).toInt()}
+        Количество бонусной железной руды: ${(metalOre * buff).toInt()}
+    """.trimIndent()
+    )
+}
