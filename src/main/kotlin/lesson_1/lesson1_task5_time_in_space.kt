33238ad367e77615f1db45e14081d8736f6f3fb7
Nordone <kaldstring@gmail.com>
2025-03-13 18:36:00 +0300

    KS-2-5-complex-rate

--- src/main/kotlin/lesson_1/lesson1_task5_time_in_space.kt
+++ src/main/kotlin/lesson_1/lesson1_task5_time_in_space.kt
@@ -1,28 +1,15 @@
 package org.example.lesson_1
 
 fun main() {
-    val secondsInSpace = 6488
-    val hoursInSpace = secondsInSpace / 3600
-    val minutesRemaining = secondsInSpace / 60 - hoursInSpace * 60
-    val secondsRemaining = secondsInSpace % (secondsInSpace / 60)
+    val totalSecondsInSpace = 6480
+    val secondsInHour = 3600
+    val minutesInHour = 60
+    val hoursInSpace = totalSecondsInSpace / secondsInHour
 
-    print("Время нахождения Юрия Гагарина в космосе: ")
-    timeBuilder(hoursInSpace, minutesRemaining, secondsRemaining)/*
-    почему-то если пихнуть метод прямо в принт ${}, то почему-то сначала печатается результат выполнения метода,
-    а потом текст
-     */
-}
+    val minutesInSpace = totalSecondsInSpace / minutesInHour - hoursInSpace * minutesInHour
+    val secondsInSpace = totalSecondsInSpace % (totalSecondsInSpace / minutesInHour)
 
-fun timeBuilder(hour: Int, minute: Int, second: Int) {
-    println("${timeFormat(hour)}:${timeFormat(minute)}:${timeFormat(second)}")
-//вспомогательный метод, чтобы распечатать красиво время и не громоздить в мэйне
+    println("${String.format("%02d", hoursInSpace)}:${String.format("%02d",minutesInSpace)}:${String.format("%02d", secondsInSpace)}")
 
 }
 
-
-fun timeFormat(num: Int): String {
-    return if (num / 10 == 0) "0$num"
-    else num.toString()/*возвращает часы\минуты\секунды в виде строки и добавляет "0" впереди, если переданное число имеет 1 символ
-    работает только в рамках задачи, чтобы не получать деление на ноль, нужно допилить условия
-    */
-} 
