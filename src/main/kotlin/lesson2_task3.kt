e32645dd0daa07435bdbd36150512dc0ae331809
Nordone <kaldstring@gmail.com>
2025-03-14 18:03:41 +0300

    KS-2-3-train-shedule

--- src/main/kotlin/lesson2_task3.kt
+++ src/main/kotlin/lesson2_task3.kt
@@ -5,10 +5,10 @@ fun main() {
     val departureHour = 9
     val departureMinute = 39
     val minutesInHour = 60
-    val travelTimeMinutes = 457 //количество минут в пути
-
+    val travelTimeMinutes = 457
     val arriveHour = departureHour + (travelTimeMinutes / minutesInHour)
     val arriveMinute = (travelTimeMinutes + departureMinute) % minutesInHour
+
     println(String.format("%2d:%2d",arriveHour,arriveMinute))
 
 
