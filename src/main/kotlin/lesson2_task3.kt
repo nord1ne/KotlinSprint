077bc5897ce5ebf0c0eb14c103f00340cc119f21
Nordone <kaldstring@gmail.com>
2025-03-14 12:32:29 +0300

    KS-2-5-complex-rate

--- src/main/kotlin/lesson2_task3.kt
+++ src/main/kotlin/lesson2_task3.kt
@@ -6,14 +6,9 @@ fun main() {
     val departureMinute = 39
     val travelTime = 457 //количество минут в пути
 
-    val arriveHour = timeFormat(departureHour + travelTime % departureHour)
-    val arriveMinute = timeFormat(travelTime % 60)
-    println("$arriveHour:$arriveMinute")
+    val arriveHour = departureHour + travelTime % departureHour
+    val arriveMinute = travelTime % 60
+    println("${String.format("%2d", arriveHour)}:${String.format("%2d", arriveMinute)}")
 
 }
 
-fun timeFormat(time: Int): String {
-    return if (time / 10 == 0)
-        "0$time"
-    else "$time"
-}
