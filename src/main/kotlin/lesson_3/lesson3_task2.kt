daf74ae79d9a8a9934591add68d715ae692e4688
Nordone <kaldstring@gmail.com>
2025-03-14 17:17:19 +0300

    KS-3-2-lastname-changes

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main() {
+    var maidenName = "Андреева"
+    var lastNameActual = "Сидорова"
+    var name = "Татьяна"
+    var patronymic = "Сергеевна"
+    val marriageAge = 22
+    val checkAge = 20
+
+    println("ФИО: $maidenName $name $patronymic, $checkAge")
+    println("ФИО: $lastNameActual $name $patronymic, $marriageAge")
+
+
+}
+
+
