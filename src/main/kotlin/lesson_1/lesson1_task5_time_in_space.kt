4799a53a2035667df1b55a38e943c9bcb419a94a
Nordone <kaldstring@gmail.com>
2025-03-15 10:52:17 +0300

    KS-2-3-train-shedule

--- src/main/kotlin/lesson_1/lesson1_task5_time_in_space.kt
+++ src/main/kotlin/lesson_1/lesson1_task5_time_in_space.kt
@@ -1,15 +1,16 @@
 package org.example.lesson_1
 
+const val MINUTES_IN_HOUR = 60
+const val SECONDS_IN_HOUR = 3600
+
 fun main() {
     val totalSecondsInSpace = 6480
-    val secondsInHour = 3600
-    val minutesInHour = 60
-    val hoursInSpace = totalSecondsInSpace / secondsInHour
-
-    val minutesInSpace = totalSecondsInSpace / minutesInHour - hoursInSpace * minutesInHour
-    val secondsInSpace = totalSecondsInSpace % (totalSecondsInSpace / minutesInHour)
+    val hoursInSpace = totalSecondsInSpace / SECONDS_IN_HOUR
+    val minutesInSpace = totalSecondsInSpace / MINUTES_IN_HOUR - hoursInSpace * MINUTES_IN_HOUR
+    val secondsInSpace = totalSecondsInSpace % (totalSecondsInSpace / MINUTES_IN_HOUR)
 
     println(String.format("%02d:%02d:%02d", hoursInSpace, minutesInSpace, secondsInSpace))
 
 }
+
 
