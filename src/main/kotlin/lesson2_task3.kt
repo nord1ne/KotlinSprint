2394fcaad32d9c85447b86fb4fae0e466bd5d49c
Nordone <kaldstring@gmail.com>
2025-03-15 10:46:37 +0300

    KS-2-3-train-shedule

--- src/main/kotlin/lesson2_task3.kt
+++ src/main/kotlin/lesson2_task3.kt
@@ -1,15 +1,17 @@
 package org.example
 
+const val MINUTES_IN_HOUR = 60
+
 fun main() {
 
     val departureHour = 9
     val departureMinute = 39
-    val minutesInHour = 60
-    val travelTimeMinutes = 457
-    val arriveHour = departureHour + (travelTimeMinutes / minutesInHour)
-    val arriveMinute = (travelTimeMinutes + departureMinute) % minutesInHour
 
-    println(String.format("%2d:%2d",arriveHour,arriveMinute))
+    val travelTimeMinutes = 457
+    val arriveHour = departureHour + (travelTimeMinutes / MINUTES_IN_HOUR)
+    val arriveMinute = (travelTimeMinutes + departureMinute) % MINUTES_IN_HOUR
+
+    println(String.format("%2d:%2d", arriveHour, arriveMinute))
 
 }
 
