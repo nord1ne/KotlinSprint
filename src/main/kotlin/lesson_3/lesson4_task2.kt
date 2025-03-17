106aabfffb529a99e37703754220c56819dafafd
Nordone <kaldstring@gmail.com>
2025-03-17 12:45:10 +0300

    KS-4-2-chess-game-decryption

--- src/main/kotlin/lesson_3/lesson4_task2.kt
+++ src/main/kotlin/lesson_3/lesson4_task2.kt
@@ -1,19 +1,22 @@
 package org.example.lesson_3
 
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+const val MAX_VOLUME = 100
+
 fun main() {
 
-    val MinWeight = 35
-    val MaxWeight = 100
-    val MaxVolume = 100
-    var categoryAverage = false
-
+    var categoryAverage: Boolean
     var cargoWeight = 20
     var cargoVolume = 80
+    categoryAverage = cargoWeight > MIN_WEIGHT && cargoWeight <= MAX_WEIGHT && cargoVolume < MAX_VOLUME
 
-    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': ${categoryAverage == cargoWeight >= MinWeight && cargoWeight < MaxWeight && cargoVolume < MaxVolume}")
+    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': $categoryAverage")
 
     cargoWeight = 50
     cargoVolume = 100
+    categoryAverage = cargoWeight > MIN_WEIGHT && cargoWeight <= MAX_WEIGHT && cargoVolume < MAX_VOLUME
 
-    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': ${categoryAverage == cargoWeight >= MinWeight && cargoWeight < MaxWeight && cargoVolume < MaxVolume}")
+    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': $categoryAverage")
+
 }
