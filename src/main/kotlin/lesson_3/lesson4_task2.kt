de99182f430bce4b0d6fe2a1c1a0d9c0f6a03193
Nordone <kaldstring@gmail.com>
2025-03-17 16:39:17 +0300

    KS-4-5-ship-depart-ability

--- src/main/kotlin/lesson_3/lesson4_task2.kt
+++ src/main/kotlin/lesson_3/lesson4_task2.kt
@@ -6,17 +6,17 @@ const val MAX_VOLUME = 100
 
 fun main() {
 
-    var categoryAverage: Boolean
+    var isCategoryAverage: Boolean
     var cargoWeight = 20
     var cargoVolume = 80
-    categoryAverage = cargoWeight > MIN_WEIGHT && cargoWeight <= MAX_WEIGHT && cargoVolume < MAX_VOLUME
+    isCategoryAverage = cargoWeight > MIN_WEIGHT && cargoWeight <= MAX_WEIGHT && cargoVolume < MAX_VOLUME
 
-    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': $categoryAverage")
+    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': $isCategoryAverage")
 
     cargoWeight = 50
     cargoVolume = 100
-    categoryAverage = cargoWeight > MIN_WEIGHT && cargoWeight <= MAX_WEIGHT && cargoVolume < MAX_VOLUME
+    isCategoryAverage = cargoWeight > MIN_WEIGHT && cargoWeight <= MAX_WEIGHT && cargoVolume < MAX_VOLUME
 
-    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': $categoryAverage")
+    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': $isCategoryAverage")
 
 }
