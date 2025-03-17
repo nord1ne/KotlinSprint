0273d9c26574fedb9c63c43aac6a63d09f5f3aed
Nordone <kaldstring@gmail.com>
2025-03-17 15:13:09 +0300

    KS-4-4-sport-exercises

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,7 +2,7 @@ package org.example.lesson_4
 
 fun main() {
 
-    var day = 5
+    var day = 6
     var canTrainHands = if (day % 2 == 1) true else false
     var canTrainBelly = if (day % 2 == 1) true else false
     var canTrainBack = if (day % 2 == 0) true else false
@@ -10,10 +10,10 @@ fun main() {
 
     println(
         """
-        Упражнения для рук:     $canTrainHands
-        Упражнения для ног:     $canTrainLegs
-        Упражнения для спины:   $canTrainBack
-        Упражнения для пресса:  $canTrainBelly
+        Упражнения для рук:    $canTrainHands
+        Упражнения для ног:    $canTrainLegs
+        Упражнения для спины:  $canTrainBack
+        Упражнения для пресса: $canTrainBelly
     """.trimIndent()
     )
 }
