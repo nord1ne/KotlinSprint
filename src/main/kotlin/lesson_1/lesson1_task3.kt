1cb6dfb49e6173ecec8fbe12263ce03e7bff568b
Nordone <kaldstring@gmail.com>
2025-03-17 12:20:40 +0300

    KS-3-5-chess-game-decryption

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main() {
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
+
+    println(
+        """
+        $hour
+        $minute
+    """.trimIndent()
+    )
+    hour = "10"
+    minute = "55"
+    print("$hour:$minute")
+}
---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_1
+
+fun main() {
+    val length: Long = 40_868_600_000L
+    val age: Byte = 27
+    val partOfDay: Float = 0.075F
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println(
+        """
+        $length,
+        $age,
+        $partOfDay,
+        $seconds,
+        $partOfYear,
+        $apogee
+    """.trimIndent()
+    )
+}
